180a8a54df671f783bdb3135054fd72efde42d53
Ganti Seetha ramakrishna <139774743+Hellokrrish@users.noreply.github.com>
2025-08-02 06:14:54 +0000

    planetweight

---
+++ HTML,CSS,JAVASCRIPT,JAVA/javascript/planetweight.js
@@ -0,0 +1,17 @@
+/*Create a weight conversion program that:
+
+Takes your weight on Earth.
+Converts it to your weight on either the moon or Mars.
+To calculate your weight on Mars:
+
+
+destination_weight=earth_weight×0.38*/
+
+
+// Write code below 💖
+
+const earthWeight = 210;
+const marsWeight = earthWeight * 0.38;
+
+console.log("Your weight on Earth is " + earthWeight + " pounds.");
+console.log("Your weight on Mars is " + marsWeight + " pounds.");
