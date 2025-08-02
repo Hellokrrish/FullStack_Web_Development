54a4fceb7a701e6131af74cd9fb27cd8199f7734
Ganti Seetha ramakrishna <139774743+Hellokrrish@users.noreply.github.com>
2025-08-02 05:10:26 +0000

    datatypes

---
+++ HTML,CSS,JAVASCRIPT,JAVA/javascript/datatypes.js
@@ -0,0 +1,19 @@
+/*Create some variables for your favorite company with the following data types:
+
+companyName, a string.
+foundingYear, a number.
+isActive, a Boolean.
+fundingAmount, undefined.
+Print them out using console.log(). */
+
+
+// Write code below 💖
+
+let companyName = "google";
+let foundingYear = 1989;
+let isActive = true;
+let fundingAmount ;
+console.log(companyName);
+console.log(foundingYear);
+console.log(isActive);
+console.log(fundingAmount);
