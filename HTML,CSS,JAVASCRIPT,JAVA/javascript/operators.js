c47938768c9fb9912536852dac5006fe7e9d0c1f
Ganti Seetha ramakrishna <139774743+Hellokrrish@users.noreply.github.com>
2025-08-02 05:58:44 +0000

    operators

---
+++ HTML,CSS,JAVASCRIPT,JAVA/javascript/operators.js
@@ -0,0 +1,23 @@
+let score = 0;        // Declare score and initialize it
+
+score = 4 + 3;        // score is now 7
+score = 4 - 3;        // score is now 1
+score = 4 * 3;        // score is now 12
+score = 4 / 3;        // score is now 1.3333
+score = 4 % 3;        // score is now 1
+
+console.log(score);   // Output: 1
+
+
+// Create a program that calculates the tip for a pizza and coke order.
+const pizza = 2.99;
+const coke = 0.99;
+let total = pizza + coke;
+let tip = total * 0.2;
+console.log(tip);     // Output: 0.796
+
+
+// Create a temperature.js program that converts a temperature from Fahrenheit (°F) to Celsius (°C).
+let fahrenheit = 68;
+temperature = (fahrenheit-32) /1.8;
+console.log(temperature);
