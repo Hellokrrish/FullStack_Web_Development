045d6b678f49b8d6f26d609ea7b330d4dd67dd13
Ganti Seetha ramakrishna <139774743+Hellokrrish@users.noreply.github.com>
2025-08-02 04:58:04 +0000

    variables

---
+++ HTML,CSS,JAVASCRIPT,JAVA/javascript/variables.js
@@ -0,0 +1,21 @@
+/*Create 4 variables for building a user profile on your favorite website:
+
+Two const variables for your first name and favorite color.
+Two let variables for your current location and mood.
+Lastly, print them out with console.log().
+
+Reassign a new value to at least one of the let variables and print it one more time with console.log().*/
+
+
+// Write code below 
+
+const firstname = "krishna";
+const color = "red";
+let location ="vizag";
+let currentmood ="very excited";
+console.log(firstname);
+console.log(color);
+console.log(location);
+console.log(currentmood);
+location= "USA";
+console.log(location);
