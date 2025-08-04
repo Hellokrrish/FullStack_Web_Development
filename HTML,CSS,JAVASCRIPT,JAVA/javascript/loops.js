17a57f8e16a93732adeda9d1cea68037bdae8874
Ganti Seetha ramakrishna <139774743+Hellokrrish@users.noreply.github.com>
2025-08-04 17:13:09 +0000

    loop

--- HTML,CSS,JAVASCRIPT,JAVA/javascript/loops.js
+++ HTML,CSS,JAVASCRIPT,JAVA/javascript/loops.js
@@ -28,3 +28,25 @@ Use the for loop to log the following message to the Console 100 times: */
 for(let i=0;i<100;i++){
   console.log("I Must Not Tell Lies");
 }
+
+/*Let's try out the continue and break keywords using even and odd numbers!
+
+Write an even-odds.js file that begins with a for loop that iterates from 1 to 50.
+
+If the iterator variable i is odd, use the continue keyword to skip this iteration. Otherwise, log the number to the Console.
+
+If the iterator variable is equal to 42, log 42 to the Console and use the break keyword to exit the loop. */
+
+
+// Write code below 💖
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 === 1) {
+    continue;
+  } else if (i === 42) {
+    console.log(i);
+    break;
+  } else {
+    console.log(i);
+  }
+}
