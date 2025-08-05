9117a7a66e93a074cf15dc88e9dc301e30b1a03c
Ganti Seetha ramakrishna <139774743+Hellokrrish@users.noreply.github.com>
2025-08-05 01:31:11 +0000

    functions

---
+++ HTML,CSS,JAVASCRIPT,JAVA/javascript/functions.js
@@ -0,0 +1,18 @@
+/*A function in JavaScript is a reusable block of code designed to perform a specific task. Functions are fundamental building blocks in JavaScript, enabling developers to organize, modularize, and reuse code effectively.  */
+
+ function addtwo(num){
+    return num+2;
+}
+const finalnumber = addtwo(7);
+console.log(finalnumber);
+
+
+
+
+const mycity ="seattle";
+      const mystate= "ap";
+      const mycountry ="india";
+      function address (mycity,mystate, mycountry) {
+      console.log("your are from"${mycity},${mystate},${mycountry});
+}
+address(mycity,mystate,mycountry);
